53f5b5b9cc2637bb84d8906d46d6016e9a7474a0
IrinaBob <kastyrkinaivl@gmail.com>
2025-09-22 10:55:57 -0400

    Modified HelloWorld Class
    Added print statement, fixed error

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
-public class HelloWord
+public class HelloWorld {
 
  public static void main(String[] args) {
 
    System.out.println("Hello, World!");
+   
+   System.out.println("Program modified, but able to revert to original version using GitHub Desktop");
+
    
   }
  
